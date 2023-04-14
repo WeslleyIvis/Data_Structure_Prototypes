e085fafea48e83ccfa8e0dd9208f15f6e6b26049
WeslleyIvis <weslley_reive@hotmail.com>
2023-04-14 03:06:57 -0300

    Stack / Decimal to Binary function

--- MainClass.java
+++ MainClass.java
@@ -4,11 +4,23 @@ public class MainClass {
     public static void main(String[] args) {
         // ArrayList<Carta> baralho = new ArrayList<Carta>();
 
-        Carro car = new Carro("Ferrari", "Asolan", 32);
-        Moto moto = new Moto("Harley", "Cacau show");
+        decimalToBinary(255);
+    }
 
-        moto.imprimir();
-        car.imprimir();
+    public static void decimalToBinary(int value) {
+        Stack stack = new Stack(value / 2);
+        String output = "";
+
+        do {
+            stack.push(value % 2);
+            value = value / 2;
+        } while (value > 0);
+
+        while (!stack.isEmpty()) {
+            output += stack.pop();
+        }
+
+        System.out.println(output);
     }
 
 }
--- POO/Carro.java
+++ POO/Carro.java
@@ -62,3 +62,58 @@ public class Carro {
         System.out.println("Modelo: " + this.modelo + "\n Marca: " + this.marca + "\n qtdPortas: " + this.qtdPortas);
     }
 }
+
+class Moto {
+    private String modelo;
+    private String marca;
+    private boolean ligado;
+    private float velocidade;
+
+    Moto(String modelo, String marca) {
+        this.modelo = modelo;
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void ligar() {
+        this.ligado = true;
+    }
+
+    public void desligar() {
+        this.ligado = false;
+    }
+
+    public void acelerar() {
+        this.velocidade += 10;
+    }
+
+    public void brecar() {
+        this.velocidade -= 10;
+
+        if (this.velocidade < 0)
+            this.velocidade = 0;
+    }
+
+    public void buzinar() {
+        System.out.println("Bi Bi");
+    }
+
+    public void imprimir() {
+        System.out.println("Modelo: " + this.modelo + "\n Marca: " + this.marca);
+    }
+}
