effc7009070cd53fce45c36843a18cc4f2126659
WeslleyIvis <weslley_reive@hotmail.com>
2023-04-24 00:46:08 -0300

    Test: Queue Sequential static

--- MainClass.java
+++ MainClass.java
@@ -7,8 +7,22 @@ public class MainClass {
         ToNPR to = new ToNPR();
 
         String value = to.npr("A + B * (C - D) * (E + F)");
-
         System.out.println(value);
+
+        Queue fila = new Queue(5);
+
+        fila.enqueue(10);
+        fila.enqueue(5);
+        fila.enqueue(3);
+        fila.enqueue(4);
+        fila.enqueue(5);
+        fila.enqueue(3);
+
+        fila.dequeue();
+        fila.dequeue();
+        fila.dequeue();
+
+        fila.write();
     }
 
     public static void decimalToBinary(int value) {
