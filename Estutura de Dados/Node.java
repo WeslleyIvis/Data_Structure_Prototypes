854207e367efc83b35931fd21c260edb874d5f14
WeslleyIvis <weslley_reive@hotmail.com>
2023-05-15 15:19:51 -0300

    node array

---
+++ Estutura de Dados/Node.java
@@ -0,0 +1,25 @@
+class Node {
+    private Object data;
+    private Node prox;
+
+    public Node(Object data, Node prox) {
+        this.data = data;
+        this.prox = prox;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Object getDado() {
+        return this.data;
+    }
+
+    public void setProximo(Node prox) {
+        this.prox = prox;
+    }
+
+    public Node getProx() {
+        return this.prox;
+    }
+}
--- MainClass.java
+++ MainClass.java
@@ -4,32 +4,7 @@ public class MainClass {
     public static void main(String[] args) {
         // ArrayList<Carta> baralho = new ArrayList<Carta>();
 
-        ToNPR to = new ToNPR();
 
-        String value = to.npr("A + B * (C - D) * (E + F)");
-        System.out.println(value);
-
-        QueueCircular fila = new QueueCircular(5);
-
-        fila.enqueue(0);
-        fila.enqueue(10);
-        fila.enqueue(20);
-        fila.enqueue(30);
-        fila.dequeue();
-        fila.dequeue();
-        fila.enqueue(40);
-        fila.enqueue(50);
-        fila.dequeue();
-        fila.dequeue();
-        fila.dequeue();
-        fila.enqueue(60);
-        fila.enqueue(70);
-        fila.dequeue();
-        fila.enqueue(80);
-        fila.enqueue(90);
-        fila.enqueue(100);
-
-        fila.write();
     }
 
     public static void decimalToBinary(int value) {
