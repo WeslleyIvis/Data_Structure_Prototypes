d7717c508daa7605e0055792645d925c6b327eba
WeslleyIvis <weslley_reive@hotmail.com>
2023-05-20 22:00:11 -0300

    Interface game jogo do bixo

--- MainClass.java
+++ MainClass.java
@@ -1,15 +1,42 @@
-import java.util.*;
+import javax.swing.*;
+import java.awt.FlowLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainClass {
     public static void main(String[] args) {
-        StackDin p1 = new StackDin();
-        FilaDin fila = new FilaDin();
+        Deck deck = new Deck();
+        Jogador mao = new Jogador();
 
-        fila.enqueue(10);
-        fila.enqueue(10);
-        fila.enqueue("Carro");
+        deck.gerarBaralho(2);
+        deck.embaralhar();
 
-        System.out.println(fila.toString());
+        JFrame frame = new JFrame("Jogo do Bixo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
+
+        JLabel cartaAtual = new JLabel("Comprou: --");
+        JLabel label = new JLabel("Valor: 0");
+        label.setPreferredSize(new Dimension(300, 300));
+
+        JButton button = new JButton("Comprar Carta");
+
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Carta cartaComprada = deck.darCarta();
+                mao.comprarCarta(cartaComprada);
+                cartaAtual.setText("" + deck.imprimirCarta(cartaComprada));
+                label.setText("Valor: " + mao.valorDaMao());
+            }
+        });
+
+        frame.getContentPane().add(cartaAtual);
+        frame.getContentPane().add(label);
+        frame.getContentPane().add(button);
+        frame.pack();
+        frame.setVisible(true);
+
     }
 
     public static void decimalToBinary(int value) {
